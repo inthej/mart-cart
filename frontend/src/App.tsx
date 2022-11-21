e6251bb2292292e3be4b7559f75295401ec84ae0
inthej <inthej12@gmail.com>
2022-11-21 20:09:48 +0900

    HTML: Header

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,15 +2,30 @@ import "./App.css";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
-import { faCoffee } from "@fortawesome/free-solid-svg-icons";
+import { faStore } from "@fortawesome/free-solid-svg-icons";
 
 function App() {
   return (
     <div className="App">
-      <FontAwesomeIcon icon={faCoffee} />
-      <header>Header</header>
-      <main>Content</main>
-      <footer>Footer</footer>
+      <header id="header">
+        <div className="header__logo">
+          <FontAwesomeIcon icon={faStore} />
+          현마트
+        </div>
+        <div className="header__searchbar">
+          <input type="text" name="searchbar" id="searchbar" />
+          <button>검색</button>
+        </div>
+      </header>
+      <main id="main">
+        <div className="main__content">
+          <section id="homeplus">HomePlus</section>
+          <section id="emart">Emart</section>
+          <section id="lottemart">LotteMart</section>
+        </div>
+        <div id="main__banner">Banner</div>
+      </main>
+      <footer id="footer">Footer</footer>
     </div>
   );
 }
