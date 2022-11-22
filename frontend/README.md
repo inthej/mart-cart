0c23be89ef4ba5c5c4af8b3771576e3349f4b8ac
inthej <inthej12@gmail.com>
2022-11-22 20:40:58 +0900

    CSS: Global setup & reset

--- frontend/README.md
+++ frontend/README.md
@@ -5,8 +5,9 @@
 ```
 $ yarn create react-app frontend --template typescript && cd frontend
 $ yarn add @fortawesome/fontawesome-svg-core @fortawesome/free-solid-svg-icons @fortawesome/react-fontawesome
+$ yarn add styled-components @types/styled-components
 ```
 
 ## Reference
 
-- Sample [http://localhost:3000](http://localhost:3000)
+- styled-components [https://wonit.tistory.com/366](https://wonit.tistory.com/366)
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,52 +1,63 @@
-import "./App.css";
+import './App.css'
 
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React from "react";
-import { faStore } from "@fortawesome/free-solid-svg-icons";
+import { ThemeProvider, createGlobalStyle } from 'styled-components'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faStore } from '@fortawesome/free-solid-svg-icons'
+import theme from './theme/theme'
+
+const Globalstyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+  }
+`
 
 function App() {
   return (
-    <div className="App">
-      {/* Header */}
-      <header id="header">
-        <div className="header__logo">
-          <FontAwesomeIcon icon={faStore} />
-          현마트
-        </div>
-        <div className="header__searchbar">
-          <input type="text" name="searchbar" id="searchbar" />
-          <button>검색</button>
-        </div>
-      </header>
-      {/* Navbar */}
-      <nav id="navbar">
-        <div className="navbar__banner">
-          <ul className="navbar__banner__list">
-            <li className="navbar__banner__list__item">베너1</li>
-            <li className="navbar__banner__list__item">베너2</li>
-          </ul>
-        </div>
-        <div className="navbar__author">만든이</div>
-      </nav>
-      {/* Main */}
-      <main id="main">
-        <section className="main__homeplus">
-          <header className="main__homeplus-header">홈플러스</header>
-          <div className="main__homeplus-content">내용</div>
-        </section>
-        <section className="main__emart">
-          <header className="main__emart-header">이마트</header>
-          <div className="main__emart-content">내용</div>
-        </section>
-        <section className="main__lottemart">
-          <header className="main__lottemart-header">롯데마트</header>
-          <div className="main__lottemart-content">내용</div>
-        </section>
-      </main>
-      {/* Footer */}
-      <footer id="footer">Footer</footer>
-    </div>
-  );
+    <ThemeProvider theme={theme}>
+      <Globalstyle />
+      <div className="App">
+        {/* Header */}
+        <header id="header">
+          <div className="header__logo">
+            <FontAwesomeIcon icon={faStore} />
+            현마트
+          </div>
+          <div className="header__searchbar">
+            <input type="text" name="searchbar" id="searchbar" />
+            <button>검색</button>
+          </div>
+        </header>
+        {/* Navbar */}
+        <nav id="navbar">
+          <div className="navbar__banner">
+            <ul className="navbar__banner__list">
+              <li className="navbar__banner__list__item">베너1</li>
+              <li className="navbar__banner__list__item">베너2</li>
+            </ul>
+          </div>
+          <div className="navbar__author">만든이</div>
+        </nav>
+        {/* Main */}
+        <main id="main">
+          <section className="main__homeplus">
+            <header className="main__homeplus-header">홈플러스</header>
+            <div className="main__homeplus-content">내용</div>
+          </section>
+          <section className="main__emart">
+            <header className="main__emart-header">이마트</header>
+            <div className="main__emart-content">내용</div>
+          </section>
+          <section className="main__lottemart">
+            <header className="main__lottemart-header">롯데마트</header>
+            <div className="main__lottemart-content">내용</div>
+          </section>
+        </main>
+        {/* Footer */}
+        <footer id="footer">Footer</footer>
+      </div>
+    </ThemeProvider>
+  )
 }
 
-export default App;
+export default App
