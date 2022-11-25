a73c6ad4bb17098a206997b8b8ec4c238a3d0168
inthej <inthej12@gmail.com>
2022-11-25 21:22:46 +0900

    HTML: Product Layout & CSS

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,20 @@ import './App.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCartShopping } from '@fortawesome/free-solid-svg-icons'
 
+const productDataList = [
+  { imgUrl: './img/product_sample.jpg', name: '오리온 고래밥 볶은약념 160G', price: 1000, unit: '10g', unitPrice: 175, },
+  { imgUrl: './img/product_sample.jpg', name: '오리온 고래밥 볶은약념 160G', price: 1000, unit: '10g', unitPrice: 175, },
+  { imgUrl: './img/product_sample.jpg', name: '오리온 고래밥 볶은약념 160G', price: 1000, unit: '10g', unitPrice: 175, },
+  { imgUrl: './img/product_sample.jpg', name: '오리온 고래밥 볶은약념 160G', price: 1000, unit: '10g', unitPrice: 175, },
+  { imgUrl: './img/product_sample.jpg', name: '오리온 고래밥 볶은약념 160G', price: 1000, unit: '10g', unitPrice: 175, },
+  { imgUrl: './img/product_sample.jpg', name: '오리온 고래밥 볶은약념 160G', price: 1000, unit: '10g', unitPrice: 175, },
+  { imgUrl: './img/product_sample.jpg', name: '오리온 고래밥 볶은약념 160G', price: 1000, unit: '10g', unitPrice: 175, },
+  { imgUrl: './img/product_sample.jpg', name: '오리온 고래밥 볶은약념 160G', price: 1000, unit: '10g', unitPrice: 175, },
+  { imgUrl: './img/product_sample.jpg', name: '오리온 고래밥 볶은약념 160G', price: 1000, unit: '10g', unitPrice: 175, },
+  { imgUrl: './img/product_sample.jpg', name: '오리온 고래밥 볶은약념 160G', price: 1000, unit: '10g', unitPrice: 175, },
+  { imgUrl: './img/product_sample.jpg', name: '오리온 고래밥 볶은약념 160G', price: 1000, unit: '10g', unitPrice: 175, },
+]
+
 function App() {
   return (
     <div className="App">
@@ -32,17 +46,107 @@ function App() {
         <div className="content">
           <section id="content__homplus">
             <header><h3>홈플러스</h3></header>
+            <div className="product-list">
+              {
+                productDataList.map(product => (
+                  <div className="product-list-item">
+                    <div className="product-item-img-wrap">
+                      <img src={product.imgUrl} id="product-img" width="100px" />
+                    </div>
+                    <div className="product-item-info">
+                      <span>{product.name}</span>
+                    </div>
+                    <div className="product-item-price-cart-wrap">
+                      <div className="product-item-price-wrap">
+                        <div className='product-item-price-wrap-price'>
+                          <strong>{product.price}</strong>원
+                        </div>
+                        <div className='product-item-price-wrap-unit'>
+                          <span>{product.unit}:{product.unitPrice}원</span>
+                        </div>
+                      </div>
+                      <div className="product-item-cart-wrap">
+                        <div className="product-item-cart-wrap-inner">
+                          <button>-</button>
+                          <input type="text" value="1" id="cartCounter"/>
+                          <button>+</button>
+                        </div>
+                        <button>카트담기</button>
+                      </div>
+                    </div>
+                  </div>
+                ))
+              }
+            </div>
           </section>
           <section id="content__emart">
             <header><h3>이마트</h3></header>
-            <div id="product-items">
-              
+            <div className="product-list">
+              {
+                productDataList.map(product => (
+                  <div className="product-list-item">
+                    <div className="product-item-img-wrap">
+                      <img src={product.imgUrl} id="product-img" width="100px" />
+                    </div>
+                    <div className="product-item-info">
+                      <span>{product.name}</span>
+                    </div>
+                    <div className="product-item-price-cart-wrap">
+                      <div className="product-item-price-wrap">
+                        <div className='product-item-price-wrap-price'>
+                          <strong>{product.price}</strong>원
+                        </div>
+                        <div className='product-item-price-wrap-unit'>
+                          <span>{product.unit}:{product.unitPrice}원</span>
+                        </div>
+                      </div>
+                      <div className="product-item-cart-wrap">
+                        <div className="product-item-cart-wrap-inner">
+                          <button>-</button>
+                          <input type="text" value="1" id="cartCounter"/>
+                          <button>+</button>
+                        </div>
+                        <button>카트담기</button>
+                      </div>
+                    </div>
+                  </div>
+                ))
+              }
             </div>
           </section>
           <section id="content__lottemart">
             <header><h3>롯데마트</h3></header>
-            <div id="product-items">
-              
+            <div className="product-list">
+              {
+                productDataList.map(product => (
+                  <div className="product-list-item">
+                    <div className="product-item-img-wrap">
+                      <img src={product.imgUrl} id="product-img" width="100px" />
+                    </div>
+                    <div className="product-item-info">
+                      <span>{product.name}</span>
+                    </div>
+                    <div className="product-item-price-cart-wrap">
+                      <div className="product-item-price-wrap">
+                        <div className='product-item-price-wrap-price'>
+                          <strong>{product.price}</strong>원
+                        </div>
+                        <div className='product-item-price-wrap-unit'>
+                          <span>{product.unit}:{product.unitPrice}원</span>
+                        </div>
+                      </div>
+                      <div className="product-item-cart-wrap">
+                        <div className="product-item-cart-wrap-inner">
+                          <button>-</button>
+                          <input type="text" value="1" id="cartCounter"/>
+                          <button>+</button>
+                        </div>
+                        <button>카트담기</button>
+                      </div>
+                    </div>
+                  </div>
+                ))
+              }
             </div>
           </section>
         </div>
